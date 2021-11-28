03b5f17b128782c160c2e56a1512f3789677eb27
sailist <sailist@outlook.com>
2021-11-28 11:05:10 +0800

    add test

--- example/value_test.py
+++ example/value_test.py
@@ -1,14 +1,18 @@
 import os
-import random
-import time
 
 from dbrecord import PDict
-from joblib import hash
+from dbrecord.idtrans import BatchIDSTrans
 
 if __name__ == '__main__':
     dic = PDict('temp1.sqlite')
-    dic['a'] = 1
-    print(dic['a'])
+
+    print(dic.setdefault('1', 2))
+    print(dic.setdefault('1', 3))
+    print(dic.setdefault('3', 3))
+    dic.flush()
+
+    ids = BatchIDSTrans('temp1.sqlite')
+
     plist = dic.to_list()
-    print(plist(1))
+    print(ids(range(10)))
     os.remove('temp1.sqlite')
---
+++ tests/test_pdict.py
@@ -0,0 +1,70 @@
+import os
+
+from dbrecord import PDict
+
+
+def test_disk():
+    dic = PDict('./disk.sqlite', apply_memory=False, apply_disk=True)
+    dic['a'] = 1
+    assert 'a' in dic
+    dic['b'] = None
+    dic.flush()
+
+    dic = PDict('./disk.sqlite', apply_memory=False, apply_disk=True)
+
+    assert dic['b'] is None
+    assert len(dic) == 2
+    os.remove('./disk.sqlite')
+
+
+def test_plist():
+    dic = PDict('./disk.sqlite', apply_memory=False, apply_disk=True)
+    dic['a'] = 1
+    dic['b'] = None
+    plist = dic.to_list()
+    assert len(plist(range(10))) == 2
+    assert plist(range(10)) == [{'a': 1}, {'b': None}]
+    dic.flush()
+    os.remove('./disk.sqlite')
+
+
+def test_disk2():
+    dic = PDict('./disk.sqlite', apply_memory=False, apply_disk=True)
+    for i in range(50000):
+        dic[f'{i}'] = i
+
+    assert len(dic) == 50000
+
+    dic = PDict('./disk.sqlite', apply_memory=False, apply_disk=True)
+    assert len(dic) == 50000
+    for i, val in enumerate(dic[[f'{i}' for i in range(50000)]]):
+        assert i == val
+
+    os.remove('./disk.sqlite')
+
+
+def test_memory():
+    dic = PDict('./disk.sqlite', apply_memory=True, apply_disk=False)
+    dic['a'] = 1
+    assert 'a' in dic
+    dic['b'] = None
+
+    assert dic['b'] is None
+    assert len(dic) == 2
+    dic = PDict('./disk.sqlite', apply_memory=True, apply_disk=False)
+    assert len(dic) == 0
+
+    assert not os.path.exists('./disk.sqlite')
+
+
+def test_dic_imp():
+    dic = PDict('./disk.sqlite', apply_memory=True, apply_disk=True)
+    assert dic.setdefault('1', 1) == 1
+    assert dic.setdefault('1', 2) == 1
+
+    dic['2'] = 2
+    assert list(dic.keys()) == ['1', '2']
+    assert list(dic.values()) == [1, 2]
+    assert list(dic.items()) == [('1', 1), ('2', 2)]
+
+    os.remove('./disk.sqlite')
