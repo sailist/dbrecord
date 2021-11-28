29d36c0d43dbe9bffdc56d4455052e204490ab17
sailist <sailist@outlook.com>
2021-11-28 11:05:27 +0800

    optimize code

---
+++ dbrecord/utils.py
@@ -0,0 +1,27 @@
+import json
+
+from joblib import hash
+
+
+def construct_tuple(*values):
+    return json.dumps(values).replace('[', '(').replace(']', ')')
+
+
+class ContainsWrap():
+    pass
+
+
+class NoneType():
+    pass
+
+
+class NoneWrap():
+    pass
+
+
+none = NoneType()
+contrain = ContainsWrap()
+
+
+def inthash(item):
+    return int(hash(item)[:8], 16)
