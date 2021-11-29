7ac2d8be469f15d901cd2b15dea53361c81e7293
sailist <sailist@outlook.com>
2021-11-29 16:05:25 +0800

    optim: count function

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ pip install dist/$(python3 install.py) --user
 
 setup(
     name='dbrecord',
-    version='0.1.7',
+    version='0.1.8',
     description='torch kit for programing your dl experiments code elegant.',
     url='https://github.com/sailist/dbrecord',
     author='sailist',
