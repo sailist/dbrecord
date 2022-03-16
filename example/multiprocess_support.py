07674b7c082b7c990fa4d329f7d71758535bfcbe
sailist <sailist@outlook.com>
2022-03-16 19:00:36 +0800

    try prove multiprocess support

--- example/multiprocess_support.py
+++ example/multiprocess_support.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 def main():
     if os.path.exists('temp.sqlite'):
         os.remove('temp.sqlite')
-    dic = PDict('temp.sqlite', cache_size=10000)
+    dic = PDict('temp.sqlite', cache_size=500)
     for i in tqdm(range(50000)):
         dic[str(i)] = i
 
@@ -22,7 +22,25 @@ def main():
     for r in as_completed(res):
         print(r.result())
 
+    from concurrent.futures import ThreadPoolExecutor, as_completed
+
+    executor = ThreadPoolExecutor(5)
+    for i in range(1000):
+        res.append(executor.submit(dic.__setitem__, f's{i}', str(i)))
+    for r in as_completed(res):
+        pass
+
+    print(len(dic))
+    dic.flush()
+
+    res = []
+    for i in range(500):
+        res.append(executor.submit(dic.get, str(i)))
+    for r in as_completed(res):
+        pass
+
     os.remove('temp.sqlite')
+
 
 if __name__ == '__main__':
     main()
