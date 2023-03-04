3ed05a0279ab160c2643a06cd20821db63f119c9
sailist <sailist@outlook.com>
2023-03-04 14:57:50 +0800

    update version 1.1.5

--- dbrecord/__init__.py
+++ dbrecord/__init__.py
@@ -3,3 +3,5 @@ from .plist import PList
 from .summary import count_table, summary_db, summary_table_struct, check_db_table_ok, count_table2
 from .utils import NoneWrap, NoneType, ContainsWrap
 from .interface import SqliteInterface, SliceView
+
+__version__ = '1.1.5'
